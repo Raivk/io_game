ee3367bbf3f2aef9fbd60c41c523ba4673b67111
raivk <thomas.lbrn76@gmail.com>
2016-11-21 12:01:14 +0100

    Better comments and change how to handle server port.

--- app.js
+++ app.js
@@ -37,7 +37,9 @@ var id = 0;
 //In a simple array which will contain all players
 var players = [];
 
+//Calculate the leaderboard and send it to all players
 function calc_leaderboard(){
+    //sort players by their score.
     players.sort(function(a,b){
         if(a.score > b.score){
             return -1;
@@ -49,6 +51,7 @@ function calc_leaderboard(){
               return 1;
         }
     });
+    //Initialize data to send with empty score preset
     var data = {
         first : "-- No score yet --",
         second : "-- No score yet --",
@@ -56,6 +59,8 @@ function calc_leaderboard(){
         fourth : "-- No score yet --",
         fifth : "-- No score yet --"
     };
+    //NEED TO DO BETTER HERE
+    //Change each attribute depending on number of players
     if(players[0] != undefined){
         data.first_id = players[0].playerId;
         data.first = "1: "+players[0].pseudo+" - "+players[0].score;
@@ -76,74 +81,92 @@ function calc_leaderboard(){
         data.fifth_id = players[4].playerId;
         data.fifth = "5: "+players[4].pseudo+" - "+players[4].score;
     }
+    //Send leaderboard
     io.emit('leaderboard_change',data);
 }
 
 //Code executed on each new connection
 io.on('connection', function (socket) {
-  calc_leaderboard();
-  socket.on('play', function(data){
-    id++;
-    socket.pid = id;
-    players.push({playerId: id, pseudo:data['pseudo'], socket: socket, score:0});
-    socket.emit('connected', { playerId: id , pseudo:data['pseudo']});
-  });
-
-  socket.on('update', function (data) {
-    socket.broadcast.emit('updated', data);
-  });
-
-  socket.on('death', function (data){
-    for(var j = 0; j < players.length; j++){
-        if(players[j].playerId == data['playerId']){
-            players.splice(j,1);
+    //new player, calculate leaderboard
+    calc_leaderboard();
+    
+    //Event received after player chose to play
+    socket.on('play', function(data){
+        //give player an id
+        id++;
+        socket.pid = id;
+        //add player to players list
+        players.push({playerId: id, pseudo:data['pseudo'], socket: socket, score:0});
+        socket.emit('connected', { playerId: id , pseudo:data['pseudo']});
+    });
+    
+    //Event received when a player updates its position
+    socket.on('update', function (data) {
+        //Send new player pos to everyone
+        socket.broadcast.emit('updated', data);
+    });
+    
+    //Event received when a player has died
+    socket.on('death', function (data){
+        //We remove the player
+        for(var j = 0; j < players.length; j++){
+            if(players[j].playerId == data['playerId']){
+                players.splice(j,1);
+            }
         }
-    }
-    for(var i = 0 ; i < players.length ; i++){
-        if(players[i].playerId == data['sent_by']){
-            players[i].score += 100;
-            calc_leaderboard();
-            players[i].socket.emit("score",{score:players[i].score});
+        //We search the killer to award points
+        for(var i = 0 ; i < players.length ; i++){
+            if(players[i].playerId == data['sent_by']){
+                players[i].score += 100;
+                calc_leaderboard();
+                players[i].socket.emit("score",{score:players[i].score});
+            }
         }
-    }
-    socket.broadcast.emit("player_death",data);
-  })
-
-  socket.on('shockwave_trigger', function (data){
-    io.emit('shockwave_triggered', data);
-  });
-
-  socket.on('been_hit', function(data){
-    for(var i = 0 ; i < players.length; i++){
-        if(players[i].playerId == data['playerId']){
-            players[i].score += 25;
-            calc_leaderboard();
-            socket.broadcast.emit("score",{score:players[i].score});
+        //Send back to everyone that a player has died
+        socket.broadcast.emit("player_death",data);
+    })
+    
+    //Someone sent a shockwave
+    socket.on('shockwave_trigger', function (data){
+        io.emit('shockwave_triggered', data);
+    });
+    
+    //Someone has been hit by a shockwave (hitten player will calculate life lost client side)
+    socket.on('been_hit', function(data){
+        //We search for the player that sent the shockwave to award points
+        for(var i = 0 ; i < players.length; i++){
+            if(players[i].playerId == data['playerId']){
+                players[i].score += 25;
+                calc_leaderboard();
+                socket.broadcast.emit("score",{score:players[i].score});
+            }
         }
-    }
-  });
-
-  socket.on('pool_score',function(data){
-    for(var i = 0; i < players.length; i++){
-        if(players[i].playerId == data['playerId']){
-            players[i].score += data['stack'] * 4;
-            calc_leaderboard();
-            players[i].socket.emit("score",{score:players[i].score});
+    });
+    
+    //Someone won score in a score pool
+    socket.on('pool_score',function(data){
+        for(var i = 0; i < players.length; i++){
+            if(players[i].playerId == data['playerId']){
+                players[i].score += data['stack'] * 4;
+                calc_leaderboard();
+                players[i].socket.emit("score",{score:players[i].score});
+            }
         }
-    }
-  });
-
-  socket.on('disconnect', function(){
-    for(var i = 0; i < players.length; i++){
-        if(socket.pid == players[i].playerId){
-            io.emit('disconnect',{pid:socket.pid});
-            players.splice(i,1);
-            calc_leaderboard();
+    });
+    
+    //Someone disconnected
+    socket.on('disconnect', function(){
+        for(var i = 0; i < players.length; i++){
+            if(socket.pid == players[i].playerId){
+                io.emit('disconnect',{pid:socket.pid});
+                players.splice(i,1);
+                calc_leaderboard();
+            }
         }
-    }
-  });
+    });
 });
 
-//server.listen(process.env.PORT);
-server.listen(8080);
-console.log("Multiplayer app listening on port 8080");
+//port = process.env.PORT
+port = 8080
+server.listen(port);
+console.log("Multiplayer app listening on port "+port);
--- public/src/game.js
+++ public/src/game.js
@@ -15,12 +15,13 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
+//Initialize the game with Quintus
 var Q = Quintus({audioSupported: [ 'wav','mp3' ]})
       .include('Sprites, Scenes, Input, 2D, Anim, Touch, UI, Audio')
       .setup({ maximize: true })
       .enableSound();
 
+//Setup controls : both azerty + qwerty + arrows
 Q.input.keyboardControls({
                            90: "up",
                            87: "up",
@@ -34,27 +35,44 @@ Q.input.keyboardControls({
                            DOWN: "down",
                            32: "fire"
                          });
+//Enable mouse controls
 Q.input.mouseControls({cursor:true});
 
+//No gravity, topdown game
 Q.gravityY = 0;
 
+//We need socket.io to enable connection with server
 require(['socket.io/socket.io.js']);
 
+//Players array
 var players = [];
+
+//Choose server IP
 //var socket = io.connect('http://wavelings.eu-2.evennode.com/');
 var socket = io.connect('localhost:8080');
+
+//Vars to stock playerId alone and player
 var selfId, player;
 
+//Required by quintus
 var objectFiles = [
   './src/player'
 ];
 
+
+//Called when users click the attack upgrade button
 function inc_atq(){
+    //Player is upgradable, avoid using css modifiers to illegaly become overpowerd
     if(player.p.upgradable){
+        //push the type of upgrade to player upgrades list
         player.p.upgrades.push(1);
+        //Add attack to the player
         player.p.damage += 10;
+        //Hide upgrade menu
         document.getElementById("upgrade_menu").style="visibility:hidden";
+        //Make player non upgradable
         player.p.upgradable = false;
+        //Reduce upgrade stack of player, if over 0, re-show menu (player didn't chose upgrade for a few levels)
         player.p.level_stack--;
         if(player.p.level_stack > 0){
             document.getElementById("upgrade_menu").style="visibility:visible";
@@ -63,6 +81,7 @@ function inc_atq(){
     }
 }
 
+//SAME AS inc_atq() with range
 function inc_range(){
     if(player.p.upgradable){
         player.p.upgrades.push(2);
@@ -77,6 +96,7 @@ function inc_range(){
     }
 }
 
+//SAME AS inc_atq() with regen
 function inc_regen(){
     if(player.p.upgradable){
         player.p.upgrades.push(3);
@@ -91,6 +111,7 @@ function inc_regen(){
     }
 }
 
+//Player clicked play button, send to server that a new player is here with its pseudo
 function play(){
     if(player == undefined){
         socket.emit('play',{pseudo:document.getElementById("pseudo").value});
@@ -101,6 +122,8 @@ function play(){
     };
 }
 
+//Change leaderboard when server says so
+//NEED TO FIND A CLEANER WAY TO DO IT (array?)
 socket.on("leaderboard_change",function(data){
     document.getElementById("score_row_1").innerHTML = data["first"];
     document.getElementById("score_row_2").innerHTML = data["second"];
@@ -139,81 +162,95 @@ socket.on("leaderboard_change",function(data){
     }
 });
 
+//SETUP STAGE
 require(objectFiles, function () {
-  function setUp (stage) {
-
+    function setUp (stage) {
+    
+    //Server sent back confirmation after play has been sent by player
     socket.on('connected', function (data) {
-      selfId = data['playerId'];
-      player = new Q.Player({ playerId: selfId, x: Math.floor((Math.random() * 5000) + 40), y: Math.floor((Math.random() * 2500) + 40), socket: socket, name: data['pseudo'] });
-      stage.insert(player);
-      document.getElementById("player_name").innerHTML = player.p.name;
-      document.getElementById("lifebar").style = "visibility:visible";
-      player.trigger('join');
-      players.push({ player: player, playerId: player.p.playerId});
-      stage.add('viewport');
+        selfId = data['playerId'];
+        //Create player and randomly place it
+        player = new Q.Player({ playerId: selfId, x: Math.floor((Math.random() * 5000) + 40), y: Math.floor((Math.random() * 2500) + 40), socket: socket, name: data['pseudo'] });
+        stage.insert(player);
+        //Display HUD
+        document.getElementById("player_name").innerHTML = player.p.name;
+        document.getElementById("lifebar").style = "visibility:visible";
+        player.trigger('join');
+        players.push({ player: player, playerId: player.p.playerId});
+        stage.add('viewport');
     });
-
+        
+    //A player as updated its position
     socket.on('updated', function (data) {
-
-      var actor = players.filter(function (obj) {
-        return obj.playerId == data['playerId'];
-      })[0];
-      if (actor) {
-        actor.player.p.x = data['x'];
-        actor.player.p.hp = data['hp'];
-        actor.player.p.y = data['y'];
-        actor.player.p.angle = data['angle'];
-        actor.player.p.sheet = data['sheet'];
-        actor.player.p.opacity = data['opacity'];
-        actor.player.p.invincible = data['invincible'];
-        actor.player.p.scale = data['scale'];
-        actor.player.p.update = true;
-        if(actor.player.p.hp <= 0){
-            actor.p.name_container.destroy();
-            actor.player.destroy();
-        }
-      } else {
-        var temp = new Q.Actor({ playerId: data['playerId'], scale: data['scale'],name: data['name'],hp: data['hp'], x: data['x'], y: data['y'], angle: data['angle'], sheet: data['sheet'], opacity: data['opacity'], invincible: data['invincible']});
-        players.push({ player: temp, playerId: data['playerId']});
-        stage.insert(temp);
-        temp.p.name_container = stage.insert(new Q.UI.Container({
-                                      y: temp.p.y - 70,
-                                      x: temp.p.x
-                                    }));
-        stage.insert(new Q.UI.Text({
-              label: temp.p.name,
-              color: "grey",
-              x: 0,
-              y: 0
+        //Search for specified player
+        var actor = players.filter(function (obj) {
+            return obj.playerId == data['playerId'];
+        })[0];
+        //FOUND
+        if (actor) {
+            actor.player.p.x = data['x'];
+            actor.player.p.hp = data['hp'];
+            actor.player.p.y = data['y'];
+            actor.player.p.angle = data['angle'];
+            actor.player.p.sheet = data['sheet'];
+            actor.player.p.opacity = data['opacity'];
+            actor.player.p.invincible = data['invincible'];
+            actor.player.p.scale = data['scale'];
+            actor.player.p.update = true;
+            if(actor.player.p.hp <= 0){
+                actor.p.name_container.destroy();
+                actor.player.destroy();
+            }
+        } else {
+            //NOT FOUND ! NEW ACTOR
+            var temp = new Q.Actor({ playerId: data['playerId'], scale: data['scale'],name: data['name'],hp: data['hp'], x: data['x'], y: data['y'], angle: data['angle'], sheet: data['sheet'], opacity: data['opacity'], invincible: data['invincible']});
+            players.push({ player: temp, playerId: data['playerId']});
+            stage.insert(temp);
+            //Container and ui.text are for actor's name
+            temp.p.name_container = stage.insert(new Q.UI.Container({
+                y: temp.p.y - 70,
+                x: temp.p.x
+            }));
+            stage.insert(new Q.UI.Text({
+                label: temp.p.name,
+                color: "grey",
+                x: 0,
+                y: 0
             }),temp.p.name_container);
-      }
+        }
     });
-
+        
+    //We won some score
     socket.on("score", function(data){
         player.p.to_upgrade += data["score"] - player.p.score;
         if(player.p.to_upgrade >= 1000){
+            //We got an upgrade ready, add to upgrade stack and display upgrade menu
             player.p.level_stack++;
             player.p.to_upgrade = 0;
             document.getElementById("upgrade_menu").style="visibility:visible";
             player.p.upgradable = true;
         }
-        //Display a feedback on screen at each upgrade.
+        //TO DO: Display a feedback on screen at each upgrade.
         player.p.score = data["score"];
         document.getElementById("score_amount").innerHTML = player.p.score;
     });
-
+        
+    //Someoe sent a shockwave (or we sent it)
     socket.on('shockwave_triggered', function (data){
+        //We sent it
         if(data['playerId'] == player.p.playerId){
             shockwave = new Q.Shockwave({damage:data['damage'], x: data['sh_x'],y: data['sh_y'],w: data['sh_w'],h: data['sh_h'], growth: data['growth']});
             shockwave.play("evolve");
         }
         else{
+            //An ennemy sent it
             shockwave = new Q.Shockwave({damage:data['damage'], type:16, collisionMask:8, sent_by: data['playerId'], x: data['sh_x'],y: data['sh_y'],w: data['sh_w'],h: data['sh_h'], growth: data['growth']});
             shockwave.play("evolve");
         }
         stage.insert(shockwave);
     })
-
+    
+    //Someone died
     socket.on('player_death',function (data){
         //Someone else died. need to kill it immediately
         var player_to_kill = players.filter(function (obj) {
@@ -224,9 +261,10 @@ require(objectFiles, function () {
             player_to_kill.player.destroy();
         }
     })
-
+    
+    //Someone disconnected
     socket.on("disconnect",function (data){
-        //Someone else died. need to kill it immediately
+        //Someone else disconnected. need to kill it immediately
         var player_to_kill = players.filter(function (obj) {
             return obj.playerId == data['pid'];
         })[0];
@@ -236,15 +274,19 @@ require(objectFiles, function () {
         }
     });
 
-  }
+}
 
-  Q.scene('arena', function (stage) {
+//Setup the scene
+Q.scene('arena', function (stage) {
     stage.collisionLayer(new Q.TileLayer({ dataAsset: '/maps/arena.json', sheet: 'tiles' }));
     var pools = new Q.TileLayer({name:"pools", type:0, dataAsset: "/maps/pools.json", sheet: "tiles"});
     stage.collisionLayer(pools);
     setUp(stage);
   });
 
+    
+//Trial to resize game after window resize
+//DOESNT REALLY WORK, SADLY :(
 //  var rtime;
 //  var timeout = false;
 //  var delta = 200;
@@ -270,7 +312,7 @@ require(objectFiles, function () {
 //      }
 //  }
 
-  var files = [
+var files = [
     '/images/tiles.png',
     '/maps/arena.json',
     '/images/player.png',
@@ -278,9 +320,10 @@ require(objectFiles, function () {
     '/images/wave.png',
     '/images/wave.json',
     '/maps/pools.json'
-  ];
+];
 
-  Q.load(files.join(','), function () {
+//Load map and tiles
+Q.load(files.join(','), function () {
     Q.sheet('tiles', '/images/tiles.png', { tilew: 40, tileh: 40 });
     Q.compileSheets('/images/player.png', '/images/player.json');
     Q.stageScene('arena', 0);
